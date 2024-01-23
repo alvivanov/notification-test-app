c0165bd6d63ce55c9faa9aef4cd73484cfdff0f8
alexanderivanov <alvivanov96@gmail.com>
2024-01-23 14:57:34 +0300

    Tweak Yii2

--- config/db.php
+++ config/db.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'class'    => \yii\db\Connection::class,
+    'dsn'      => printf('pgsql:host=%;dbname=%s', env('DB_HOST'), env('DB_NAME')),
+    'username' => env('DB_USERNAME'),
+    'password' => env('DB_PASSWORD'),
+    'charset'  => 'utf8',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
